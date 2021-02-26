cb25083275bec454e96fca96fb155efbfc6e875b
shareef <nadeemshareef934@gmail.com>
2021-02-26 17:20:31 +0530

    add firestore to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
+import { db, auth } from "./firebase";
+
 function App() {
+    const [testing, setTesting] = useState<any[]>([]);
+    useEffect(() => {
+        console.log(db);
+        getTesting();
+    }, []);
+
+    const getTesting = () => {
+        db.collection("testing").onSnapshot((querySnapShot) => {
+            setTesting(
+                querySnapShot.docs.map((doc) => ({
+                    name: doc.data().name,
+                    id: doc.data().id,
+                }))
+            );
+        });
+    };
+
     return (
         <>
             <h1 className="text-red-100">Hello</h1>
             <h1>World</h1>
+            {testing.map((tests) => (
+                <div key={tests.id}>
+                    <h1>{tests.name}</h1>
+                    <h1>{tests.id}</h1>
+                </div>
+            ))}
         </>
     );
 }
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const app = {
+    apiKey: "AIzaSyCdNLyy2P8YVXV_9ZtKMMFZiWDJmmsKh7M",
+    authDomain: "onlineounotes.firebaseapp.com",
+    projectId: "onlineounotes",
+    storageBucket: "onlineounotes.appspot.com",
+    messagingSenderId: "567815505682",
+    appId: "1:567815505682:web:54233388f6633d27a3d5cb",
+    measurementId: "G-HN50T8CSK6",
+};
+// Initialize Firebase
+firebase.initializeApp(app);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
+export default app;
