ae0d1f05adce41c036d1f5c0519eb92076ca34c1
shareef <nadeemshareef934@gmail.com>
2021-02-28 15:07:41 +0530

    Add PrivateRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/login";
 import { Notes } from "./components/Notes";
+import { PrivateRoute } from "./components/PrivateRoute";
 import Selection from "./components/Selection";
 import { AuthProvider } from "./contexts/AuthContext";
 
@@ -13,7 +14,8 @@ function App() {
                     <Switch>
                         <Route exact path="/" component={Home} />
                         <Route path="/login" component={Login} />
-                        <Route path="/selection" component={Selection} />
+
+                        <PrivateRoute component={Selection} path="/selection" />
                         <Route path="/notes/:year/:sem" component={Notes} />
                         {/* Place a route at the bottom with path "/" under switch to catch 404 page */}
                         <Route path="/" render={() => <h1>404</h1>} />
---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,22 @@
+import { PropsWithChildren, FC } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+interface Props
+    extends PropsWithChildren<{
+        component: FC;
+        path: string;
+    }> {}
+
+export const PrivateRoute: FC<Props> = ({ component: Component, path }) => {
+    const { currentUser } = useAuth();
+
+    return (
+        <Route>
+            {(props) => {
+                console.log(props);
+                return currentUser ? <Component /> : <Redirect to="/" />;
+            }}
+        </Route>
+    );
+};
